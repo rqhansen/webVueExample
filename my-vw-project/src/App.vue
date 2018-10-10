21675d1a319a91e4da6fa0ebaac15860c278c0e7
rqhansen <1508477509@qq.com>
2018-10-10 23:48:59 +0800

    vw适配

---
+++ my-vw-project/src/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style lang="postcss">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+img {
+  width: 100%;
+  height: auto;
+  vertical-align: top;
+  content: normal !important;
+}
+  [aspectratio] {
+	  position: relative;
+  }
+  [aspectratio]::before {
+	  content: '';
+	  display: block;
+	  width: 1px;
+	  margin-left: -1px;
+	  height: 0;
+  }
+
+  [aspectratio-content] {
+	  position: absolute;
+	  top: 0;
+	  left: 0;
+	  right: 0;
+	  bottom: 0;
+	  width: 100%;
+	  height: 100%;
+  }
+  [flexContainer] {//全局属性
+    display: flex;
+    width: 750px;
+  }
+</style>
