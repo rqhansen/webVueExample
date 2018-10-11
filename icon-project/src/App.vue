85e47f3fbdcf4d3a2068162d2211f9d3b5a60b71
rq <1508477509@qq.com>
2018-10-11 20:04:35 +0800

    修改测试css

--- icon-project/src/App.vue
+++ icon-project/src/App.vue
@@ -1,9 +1,10 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <img src="./assets/aa.jpg" alt="">
-     <div class="icon-wrap">
-      <svg class="icon" aria-hidden="true">
+    <!-- <img src="./assets/logo.png">
+    <img src="./assets/aa.jpg" alt=""> -->
+    <div class="icon-wrap">
+      <svg class="icon"
+           aria-hidden="true">
         <use xlink:href="#icon-left"></use>
       </svg>
     </div>
@@ -22,25 +23,34 @@ export default {
 *使用less需要安装less,less-loader
 */
 // @import '../static/flex/fontSize.less';//字体适配
+* {
+    box-sizing: border-box;
+}
+
 #app {
-  position:relative;
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-  .px2px(font-size, 32);//字体适配
+    position: relative;
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+    .px2px(font-size, 20px); //字体适配
 }
 //通用icon样式
 .icon {
-   width: 1em;
-   height: 1em;
-   vertical-align: -0.15em;
-   fill: currentColor;
-   overflow: hidden;
+    width: 1em;
+    height: 1em;
+    vertical-align: -0.15em;
+    fill: currentColor;
+    overflow: hidden;
 }
-.icon-wrap{
-    color:@primary-color;
+.icon-wrap {
+    height: 2rem;
+    width: 2rem;
+    border: 1px solid blue; //border直接用
+    border-radius: 12px;
+    color: @primary-color;
+    .px2px(font-size,26px); //计算字体
 }
 </style>
---
+++ icon-project/src/components/Home.vue
@@ -0,0 +1,26 @@
+
+<template>
+  <div class="wrap">
+    <div class="home">我是内容</div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="less" scoped>
+.wrap {
+    // .home {
+    width: 3rem;
+    height: 1rem;
+    line-height: 1rem;
+    border: 1px solid red;
+    border-radius: 12px;
+    margin: 50px auto;
+    .px2px(font-size,22px);
+}
+</style>
+
