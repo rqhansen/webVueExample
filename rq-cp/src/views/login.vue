3a4647acbbc5ee4091332b14d6ec796eb66568d6
rq <1508477509@qq.com>
2019-01-06 19:59:48 +0800

    ''

--- rq-cp/src/views/login.vue
+++ rq-cp/src/views/login.vue
@@ -12,8 +12,28 @@
       <!-- 三角图案 -->
       <div class="login-triangle"></div>
       <!-- 表单 -->
-      <div>
-
+      <div class="login-field">
+        <div class="field-item"
+             :class="{'active':itemIndex===0}">
+          <input type="text"
+                 maxlength="12"
+                 min="6"
+                 checked
+                 @focus="itemIndex=0"
+                 @blur="itemIndex=-1"
+                 placeholder="请输入用户名">
+          <svg-icon icon-class="close_eyes"></svg-icon>
+        </div>
+        <div class="field-item"
+             :class="{'active':itemIndex===1}">
+          <input type="password"
+                 maxlength="12"
+                 min="6"
+                 @focus="itemIndex=1"
+                 @blur="itemIndex=-1"
+                 placeholder="请输入密码">
+          <svg-icon icon-class="open_eyes"></svg-icon>
+        </div>
       </div>
     </section>
   </div>
@@ -21,16 +41,25 @@
 
 <script>
 export default {
-  name: 'login'
+  name: 'login',
+  data () {
+    return {
+      itemIndex: -1,
+      userInfo: {
+        username: '',
+        phone: ''
+      }
+    }
+  }
 }
 </script>
 
 <style  lang="scss" scoped>
+@import "../assets/scss/animation.scss";
 .rq-login {
   .login-logo {
     width: 100%;
     height: 184px;
-    padding-top: 46px;
     background-color: #ec0022;
     text-align: center;
   }
@@ -40,6 +69,58 @@ export default {
     border-top: 102px solid #ec0022;
     border-left: 375px solid transparent;
     border-right: 375px solid transparent;
+  }
+  .login-field {
+    position: relative;
+    top: -100px;
+    z-index: 2;
+    width: 620px;
+    height: 590px;
+    padding: 60px 20px;
+    box-shadow: 0 0.15rem 0.7rem 0.15rem #dfdfdf;
+    border-radius: 20px;
+    margin: 0 auto;
+    background-color: #fff;
+    font-size: 30px;
+  }
+  .field-item {
+    position: relative;
+    height: 76px;
+    padding: 15px 30px 15px 0;
+    margin-bottom: 36px;
+    input {
+      width: 100%;
+      height: 100%;
+      color: #222;
+      caret-color: #ec0022;
+    }
+    &:after {
+      position: absolute;
+      display: block;
+      content: "";
+      left: 0;
+      top: 75px;
+      width: 100%;
+      height: 1px;
+      background-color: #efefef;
+    }
+    &.active {
+      &:after {
+        $an1: (
+          transform: scaleX(0)
+        );
+        $an2: (
+          transform: scaleX(1)
+        );
+        $transWidth: (
+          0%: $an1,
+          100%: $an2
+        );
+        @include keyframes(transWidth, $transWidth);
+        @include animation(transWidth 0.2s);
+        background-color: #ec0022;
+      }
+    }
   }
 }
 </style>
